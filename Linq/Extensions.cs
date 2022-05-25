9f3e982a37829623589aad2cdaddc6b5b2702a72
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2022-05-25 13:46:04 +0200

    Extensions

---
+++ Linq/Extensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    internal static class Extensions
+    {
+
+        internal static void AddYears(this Person person, int years)
+        {
+            person.Age += years;
+        } 
+        
+        internal static void AddOwnMethod(this List<Person> persons)
+        {
+
+        }  
+        
+        internal static void AddOwnMethod(this IEnumerable<Person> persons)
+        {
+
+        }
+
+        internal static void Print(this int num) => Console.WriteLine(num);
+    }
+}
--- Linq/Program.cs
+++ Linq/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Linq
 {
@@ -6,7 +8,46 @@ namespace Linq
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           var persons = GetPersons().ToList();
+           persons.AddOwnMethod(); 
+            
+           var persons2 = GetPersons();
+           persons2.AddOwnMethod(); 
+            
+            var persons3 = GetPersons().ToArray();
+           persons3.AddOwnMethod();
+
+           var p = persons.ToList()[0];
+           p.AddYears(5);
+
+            int x = 5;
+            x.Print();
+
         }
+
+
+        private static IEnumerable<Person> GetPersons()
+        {
+            return new List<Person>
+                {
+                new("Nisse", 20),
+                new("Nisse", 21),
+                new("Nisse", 22),
+                new("Nisse", 23),
+                new("Nisse", 24),
+                new("Nisse", 24),
+                new("Nisse", 26),
+                new("Pelle" ,65),
+                new("Pelle" ,60),
+                new("Pelle" ,62),
+                new("Olle" , 66),
+                new("Sara" , 54),
+                new("Ida" ,  36),
+                new("Fia",   45),
+                new("Lisa",   45),
+                new("Sophia-Magdalena", 32),
+            };
+        }
+
     }
 }
